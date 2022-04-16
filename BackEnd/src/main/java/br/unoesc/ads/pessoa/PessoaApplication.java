3122ffcbc03ed35787a43c11297f10471ba67218
Rangel Douglas Muniz <rangel.d.muniz@gmail.com>
2022-04-16 20:33:57 -0300

    Inicio

---
+++ BackEnd/src/main/java/br/unoesc/ads/pessoa/PessoaApplication.java
@@ -0,0 +1,13 @@
+package br.unoesc.ads.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaApplication.class, args);
+	}
+
+}
